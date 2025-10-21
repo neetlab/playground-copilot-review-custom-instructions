2ba31cd94e3bcf34b70b6673b65f3bd97aeb319f
Ryo Igarashi <n33t5hin@gmail.com>
2025-10-21 14:30:50 +0900

    Test commit for checking if Copilot review works

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
 import Link from "next/link";
-import { Base, Container, Header, PageHeading, Stack, TextLink } from "smarthr-ui";
+import { Base, Container, Header, PageHeading, Stack, TextLink, Table, Checkbox, Td, Th, Input } from "smarthr-ui";
+
+type User = {
+  id: number;
+  name: string;
+}
+
+const data: User[] = [
+  {
+    id: 1,
+    name: "山田太郎"
+  },
+  {
+    id: 2,
+    name: "山田花子"
+  },
+]
 
 export default function Home() {
   return (
@@ -16,6 +32,27 @@ export default function Home() {
             </TextLink>
           </Base>
         </Stack>
+
+        <Input type="text" />
+
+        <Table>
+          <thead>
+            <tr>
+              <Th>選択</Th>
+              <Th>ID</Th>
+              <Th>氏名</Th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((user) => (
+              <tr>
+                <Td><Checkbox /></Td>
+                <Td>{user.id}</Td>
+                <Td>{user.name}</Td>
+              </tr>             
+            ))} 
+          </tbody>
+        </Table>
       </Container>
     </div>
   );
